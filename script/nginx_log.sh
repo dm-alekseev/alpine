9996779b0480c979175864651fbb488717c47be4
dalekseev <dmitrij.alekseev@asstra.com>
2025-04-10 09:49:42 +0300

    modified:   docker-compose.yml 	new file:   log/400.error 	new file:   log/500.error 	new file:   log/access.log 	new file:   log/access_redblue.log 	new file:   log/error.log 	new file:   log/nginx.log 	deleted:    script/nginx_log.sh 	new file:   script/script.sh

--- script/nginx_log.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash/
-while  true 
-  do
-    cat /var/log/nginx/error.log /var/log/nginx/access.log > /var/log/nginx/nginx.log
-    grep -w HTTP\/1.1\"\ \4.. /var/log/nginx/nginx.log > /var/log/nginx/400.error
-    grep -w HTTP\/1.1\"\ \5.. /var/log/nginx/nginx.log > /var/log/nginx/500.error
-  sleep 5
-done
